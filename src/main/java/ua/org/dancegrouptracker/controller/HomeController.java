037375995f02ad17057ee182191251998ab5fb11
sevladmain <sevlad.main@gmail.com>
2017-02-11 14:35:41 +0200

    Adding spring mvc config

--- src/main/java/ua/org/dancegrouptracker/controller/HomeController.java
+++ src/main/java/ua/org/dancegrouptracker/controller/HomeController.java
@@ -1,7 +1,15 @@
 package ua.org.dancegrouptracker.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by SeVlad on 11.02.2017.
  */
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "String";
+    }
 }
