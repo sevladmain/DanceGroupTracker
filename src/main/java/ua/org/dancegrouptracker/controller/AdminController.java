0a1775b4170054e3a22f90002ca0312df211fe9f
sevladmain <sevlad.main@gmail.com>
2017-03-01 21:36:34 +0200

    Fixed error when css / js not be allowed

--- src/main/java/ua/org/dancegrouptracker/controller/AdminController.java
+++ src/main/java/ua/org/dancegrouptracker/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class AdminController {
-    @RequestMapping(name="/admin")
+    @RequestMapping("/admin")
     public String getAdminPage(Model model){
         return "admin";
     }
--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -8,3 +8,6 @@ create table users(
       authority varchar_ignorecase(50) not null,
       constraint fk_authorities_users foreign key(username) references users(username));
       create unique index ix_auth_username on authorities (username,authority);
+
+insert into users values('user', 'pwd', true);
+insert into authorities values('user', 'ROLE_ADMIN');
