7f6470332622b85677128e2ddd478879073c1a37
sevladmain <sevlad.main@gmail.com>
2017-03-08 12:13:51 +0200

    Added authorities

---
+++ src/main/java/ua/org/dancegrouptracker/model/Authorities.java
@@ -0,0 +1,16 @@
+package ua.org.dancegrouptracker.model;
+
+/**
+ * Created by SeVlad on 08.03.2017.
+ */
+public class Authorities {
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
