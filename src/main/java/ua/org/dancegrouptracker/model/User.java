3846c1e0a6b31b91f9a800884e629dd401c71970
sevladmain <sevlad.main@gmail.com>
2017-03-08 11:55:56 +0200

    Added user model

---
+++ src/main/java/ua/org/dancegrouptracker/model/User.java
@@ -0,0 +1,11 @@
+package ua.org.dancegrouptracker.model;
+
+/**
+ * Created by SeVlad on 08.03.2017.
+ */
+public class User {
+    // TODO: add username, password, userroles, useremail
+    // TODO: add getters and setters
+    // TODO: Database support
+    // TODO: password encoding
+}
