dbba5c70412de186038d9b8d5ffd1c703c38b799
sevladmain <sevlad.main@gmail.com>
2017-03-08 12:06:58 +0200

    Added username, password, userroles, useremail in User

--- src/main/java/ua/org/dancegrouptracker/model/User.java
+++ src/main/java/ua/org/dancegrouptracker/model/User.java
@@ -1,10 +1,22 @@
 package ua.org.dancegrouptracker.model;
 
+import java.sql.Date;
+
 /**
  * Created by SeVlad on 08.03.2017.
  */
 public class User {
-    // TODO: add username, password, userroles, useremail
+    private String username;
+
+    private String password;
+
+    private String userRole;
+
+    private boolean enabled;
+
+    private String email;
+
+    private Date dateRegister;
     // TODO: add getters and setters
     // TODO: Database support
     // TODO: password encoding
