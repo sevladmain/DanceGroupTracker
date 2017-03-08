dcd70d675322d798cb4bbaabbdef74cb01eb6fe9
sevladmain <sevlad.main@gmail.com>
2017-03-08 12:08:04 +0200

    added getters and setters

--- src/main/java/ua/org/dancegrouptracker/model/User.java
+++ src/main/java/ua/org/dancegrouptracker/model/User.java
@@ -17,7 +17,55 @@ public class User {
     private String email;
 
     private Date dateRegister;
-    // TODO: add getters and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateRegister() {
+        return dateRegister;
+    }
+
+    public void setDateRegister(Date dateRegister) {
+        this.dateRegister = dateRegister;
+    }
+
     // TODO: Database support
     // TODO: password encoding
 }
