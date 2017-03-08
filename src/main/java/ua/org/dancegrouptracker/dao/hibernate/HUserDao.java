d54a5d3a860635ea17ee83d8dbda1a0b1006f02b
sevladmain <sevlad.main@gmail.com>
2017-03-08 13:22:22 +0200

    Added HUserDao

---
+++ src/main/java/ua/org/dancegrouptracker/dao/hibernate/HUserDao.java
@@ -0,0 +1,33 @@
+package ua.org.dancegrouptracker.dao.hibernate;
+
+import ua.org.dancegrouptracker.dao.UserDao;
+import ua.org.dancegrouptracker.model.User;
+
+import java.util.List;
+
+/**
+ * Created by SeVlad on 08.03.2017.
+ */
+// TODO: perform test on HUserDao
+// TODO: implement methods in HUserDao
+public class HUserDao implements UserDao {
+    @Override
+    public User read(String id) {
+        return null;
+    }
+
+    @Override
+    public String saveOrUpdate(User transientObject) {
+        return null;
+    }
+
+    @Override
+    public void delete(User persistentObject) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
