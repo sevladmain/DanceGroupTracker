3e5ff67302864897ab6d4ce880b06edb4566fa9b
sevladmain <sevlad.main@gmail.com>
2017-03-08 12:40:33 +0200

    Added authoritiesDao interface

---
+++ src/main/java/ua/org/dancegrouptracker/dao/AuthoritiesDao.java
@@ -0,0 +1,9 @@
+package ua.org.dancegrouptracker.dao;
+
+import ua.org.dancegrouptracker.model.Authorities;
+
+/**
+ * Created by SeVlad on 08.03.2017.
+ */
+public interface AuthoritiesDao extends GenericDao<Authorities, String> {
+}
