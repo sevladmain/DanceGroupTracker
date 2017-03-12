5aa3f2a868148e9320d4389b0e0e87fd12292ccd
sevladmain <sevlad.main@gmail.com>
2017-03-12 14:50:00 +0200

    Fixed test sql

--- src/main/resources/scheme-test.sql
+++ src/main/resources/scheme-test.sql
@@ -5,13 +5,12 @@ create table users(
       dateregister DATE not null);
 
 create table roles (
-      id NUMBER not null primary key,
-      role_name varchar_ignorecase(50) not null,
-      create unique index ix_auth_username on userRole (username,authority);
+      id INTEGER not null primary key,
+      role_name varchar_ignorecase(50) not null);
 
   create table userRole (
       username varchar_ignorecase(50) not null,
-      role_id number not null,
-      constraint fk_authorities_users foreign key(username) references users(username));
+      role_id INTEGER not null,
+      constraint fk_authorities_users foreign key(username) references users(username),
       constraint fk_authorities_roles foreign key(role_id) references roles(id));
       create unique index ix_auth_username on userRole (username,role_id);
