81dd31e68427b033b44bdd979e75a6deb92dbee8
sevladmain <sevlad.main@gmail.com>
2017-03-12 11:38:16 +0200

    Added test class for HUserDao and Mockito dependency to maven

---
+++ src/test/java/ua/org/dancegrouptracker/dao/hibernate/HUserDaoTest.java
@@ -0,0 +1,16 @@
+package ua.org.dancegrouptracker.dao.hibernate;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SeVlad on 12.03.2017.
+ */
+public class HUserDaoTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
