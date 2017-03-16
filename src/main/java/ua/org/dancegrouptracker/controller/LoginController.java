6e3475350bc9c2e8e1bddf5955dc3fb38892a59b
sevladmain <sevlad.main@gmail.com>
2017-03-16 21:34:52 +0200

    Added mapping form register page in LoginController

--- src/main/java/ua/org/dancegrouptracker/controller/LoginController.java
+++ src/main/java/ua/org/dancegrouptracker/controller/LoginController.java
@@ -4,9 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import ua.org.dancegrouptracker.model.Roles;
+import ua.org.dancegrouptracker.model.User;
+import ua.org.dancegrouptracker.services.RolesService;
+import ua.org.dancegrouptracker.services.UserService;
 
+import javax.validation.Valid;
+import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.Locale;
 
 /**
@@ -17,6 +26,12 @@ public class LoginController {
 
     @Autowired
     private MessageSource messageSource;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RolesService rolesService;
 
     @RequestMapping("/login")
     public String login(@RequestParam(value="error", required = false) String error,
@@ -32,5 +47,23 @@ public class LoginController {
             model.addAttribute("msg", message);
         }
         return "login";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerUser(@Valid User user, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "login";
+        }
+
+        Roles role = rolesService.getRolesById(2L);
+        user.setAuthority(role);
+        user.setEnabled(true);
+
+        if(userService.getUserByUsername(user.getUsername()) != null){
+            result.rejectValue("username", "DuplicateKey.user.username");
+            return "login";
+        }
+        userService.saveOrUpdateUser(user);
+        return "/";
     }
 }
