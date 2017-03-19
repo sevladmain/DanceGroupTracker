b0a64ed38bc102bdb7d0ec033ba5b64f035c6ba2
sevladmain <sevlad.main@gmail.com>
2017-03-19 13:04:30 +0200

    Added LoginController first test that work

--- src/main/java/ua/org/dancegrouptracker/controller/LoginController.java
+++ src/main/java/ua/org/dancegrouptracker/controller/LoginController.java
@@ -38,26 +38,6 @@ public class LoginController {
         return messageSource;
     }
 
-    public UserService getUserService() {
-        return userService;
-    }
-
-    public RolesService getRolesService() {
-        return rolesService;
-    }
-
-    public void setMessageSource(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
-
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
-    public void setRolesService(RolesService rolesService) {
-        this.rolesService = rolesService;
-    }
-
     @RequestMapping("/login")
     public String login(@RequestParam(value="error", required = false) String error,
                         @RequestParam(value="logout", required = false) String logout,
--- src/test/java/ua/org/dancegrouptracker/controller/LoginControllerTest.java
+++ src/test/java/ua/org/dancegrouptracker/controller/LoginControllerTest.java
@@ -3,58 +3,59 @@ package ua.org.dancegrouptracker.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import ua.org.dancegrouptracker.model.User;
 import ua.org.dancegrouptracker.services.UserService;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Created by SeVlad on 18.03.2017.
+ * Created by SeVlad on 19.03.2017.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:spring-mvc-config.xml"})
-@WebAppConfiguration
-public class SecurityAccessTest {
-
-    @Autowired
-    private WebApplicationContext context;
-
-    private MockMvc mockMvc;
+@RunWith(MockitoJUnitRunner.class)
+public class LoginControllerTest {
 
     @Mock
     private UserService userService;
 
+    @InjectMocks
+    private LoginController controller;
+
+    private MockMvc mockMvc;
+
     @Before
-    public void setup(){
-        LoginController loginController = (LoginController) context.getBean("loginController");
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).
-                apply(springSecurity()).build();
+    public void setUp() throws Exception {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setViewResolvers(viewResolver)
+                .build();
     }
 
     @Test
-    public void mainPageIsAvaliableForAllUser() throws Exception {
-        mockMvc.perform(get("/").with(anonymous()))
-                .andExpect(status().isOk());
-        verifyNoMoreInteractions(userService);
-    }
-
-    @Test
-    public void whenLoginPageThatSetupUserForm() throws Exception {
-        mockMvc.perform(get("/login").with(anonymous()))
+    public void whenLoginPageThenEmptyUser() throws Exception {
+        mockMvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("user", is(new User())))
                 .andExpect(model().attribute("isRegister",is(false)));
