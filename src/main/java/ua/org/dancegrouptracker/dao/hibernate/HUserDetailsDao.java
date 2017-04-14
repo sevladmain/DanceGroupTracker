363de6c2aa07b5635a1dff4234e797a0ce82b76d
sevladmain <sevlad.main@gmail.com>
2017-04-14 20:45:57 +0300

    Added empty class HUserDetailsDao

---
+++ src/main/java/ua/org/dancegrouptracker/dao/hibernate/HUserDetailsDao.java
@@ -0,0 +1,31 @@
+package ua.org.dancegrouptracker.dao.hibernate;
+
+import ua.org.dancegrouptracker.dao.UserDetailsDao;
+import ua.org.dancegrouptracker.model.UserDetails;
+
+import java.util.List;
+
+/**
+ * Created by SeVlad on 14.04.2017.
+ */
+public class HUserDetailsDao implements UserDetailsDao {
+    @Override
+    public UserDetails read(String id) {
+        return null;
+    }
+
+    @Override
+    public String saveOrUpdate(UserDetails transientObject) {
+        return null;
+    }
+
+    @Override
+    public void delete(UserDetails persistentObject) {
+
+    }
+
+    @Override
+    public List<UserDetails> getAll() {
+        return null;
+    }
+}
