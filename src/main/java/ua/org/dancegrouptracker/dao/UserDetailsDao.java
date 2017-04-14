5aa1151fb85379a7e91809987c9715a07ad3cb76
sevladmain <sevlad.main@gmail.com>
2017-04-14 20:43:12 +0300

    Added UserDetailsDao interface

---
+++ src/main/java/ua/org/dancegrouptracker/dao/UserDetailsDao.java
@@ -0,0 +1,9 @@
+package ua.org.dancegrouptracker.dao;
+
+import ua.org.dancegrouptracker.model.UserDetails;
+
+/**
+ * Created by SeVlad on 14.04.2017.
+ */
+public interface UserDetailsDao extends GenericDao<UserDetails, String> {
+}
