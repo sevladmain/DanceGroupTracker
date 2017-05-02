569c5435fb1f3e9ebf060a8d1d98e2348d9ba198
sevladmain <sevlad.main@gmail.com>
2017-05-02 14:09:41 +0300

    Added GroupService

---
+++ src/main/java/ua/org/dancegrouptracker/services/GroupService.java
@@ -0,0 +1,43 @@
+package ua.org.dancegrouptracker.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import ua.org.dancegrouptracker.dao.GroupDao;
+import ua.org.dancegrouptracker.model.Group;
+
+import java.util.List;
+
+/**
+ * Created by SeVlad on 02.05.2017.
+ */
+public class GroupService {
+    @Autowired
+    private GroupDao groupDao;
+
+    @Transactional
+    public Long saveOrUpdateGroup(Group group){
+        return groupDao.saveOrUpdate(group);
+    }
+
+    @Transactional
+    public void deleteGroup(Group group){
+        if(findGroupById(group.getId()) != null){
+            groupDao.delete(group);
+        }
+    }
+
+    @Transactional
+    public Group findGroupById(Long id) {
+        return groupDao.read(id);
+    }
+
+    @Transactional
+    public List<Group> getAllGroups(){
+        return groupDao.getAll();
+    }
+
+    @Transactional
+    public List<Group> findGroupByName(String name){
+        return groupDao.getGroupByName(name);
+    }
+}
