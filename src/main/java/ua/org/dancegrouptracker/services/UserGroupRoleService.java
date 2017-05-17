5af9419a946eb164cae34f540f75b06e894285eb
sevladmain <sevlad.main@gmail.com>
2017-05-17 20:42:54 +0300

    Adding CRUD function for UserGroupRoleService #DGT-20

--- src/main/java/ua/org/dancegrouptracker/services/UserGroupRoleService.java
+++ src/main/java/ua/org/dancegrouptracker/services/UserGroupRoleService.java
@@ -1,7 +1,36 @@
 package ua.org.dancegrouptracker.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import ua.org.dancegrouptracker.dao.UserGroupRoleDao;
+import ua.org.dancegrouptracker.model.UserGroupRole;
+
+import java.util.List;
+
 /**
  * Created by SeVlad on 16.05.2017.
  */
 public class UserGroupRoleService {
+    @Autowired
+    private UserGroupRoleDao userGroupRoleDao;
+
+    @Transactional
+    public Long saveOrUpdateGroup(UserGroupRole userGroupRole) {
+        return userGroupRoleDao.saveOrUpdate(userGroupRole);
+    }
+
+    @Transactional
+    public void deleteGroup(UserGroupRole userGroupRole) {
+        userGroupRoleDao.delete(userGroupRole);
+    }
+
+    @Transactional
+    public UserGroupRole findGroupById(Long id) {
+        return userGroupRoleDao.read(id);
+    }
+
+    @Transactional
+    public List<UserGroupRole> getAllGroups() {
+        return userGroupRoleDao.getAll();
+    }
 }
