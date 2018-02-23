e36752a774af4cc723cdb350ae29043551925815
sevlad <sevlad.main@gmail.com>
2018-02-23 22:48:39 +0200

    Changing configurations: delete spring xml config, migration to java config

--- src/main/java/ua/org/dancegrouptracker/AppConfig.java
+++ src/main/java/ua/org/dancegrouptracker/AppConfig.java
@@ -1,9 +1,17 @@
 package ua.org.dancegrouptracker;
 
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource({"classpath:spring-mvc-config.xml", "classpath:spring-database-config.xml"})
+@ImportResource({"classpath:spring-mvc-config.xml"})
+@ComponentScan
 public class AppConfig {
+    @Bean
+    public PhysicalNamingStrategyStandardImpl namingStrategy() {
+        return new PhysicalNamingStrategyStandardImpl();
+    }
 }
